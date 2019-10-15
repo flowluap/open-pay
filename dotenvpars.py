6a42e046f21fb48e87980dceae07a755e1dd968c
flowluap <github@flowluap.com>
2019-10-15 15:41:21 +0200

    few chnages

--- dotenvpars.py
+++ dotenvpars.py
@@ -1,10 +1,11 @@
 def rewrite(key, value):
     try:
-        with open("/home/pi/jufö/.env","r+") as f:
-            for line in f.readlines():
-                if key in line.split("=")[0]:
-                    f.write("{}={}\n".format(line.split("=")[0], value))
-                else:
-                    f.write(line)
+        with open("/home/pi/jufö/.env","r") as f:
+            with open("/home/pi/jufö/.env","w") as w:
+                for line in f.readlines():
+                    if key in line.split("=")[0]:
+                        w.write("{}={}\n".format(line.split("=")[0], value))
+                    else:
+                        w.write(line)
     except Exception as e:
         print(e)
