eba9ee6597c4a80136c9f964ee6017009cbd808a
flowluap <github@flowluap.com>
2019-10-15 14:58:47 +0200

    added except

--- dotenvpars.py
+++ dotenvpars.py
@@ -1,10 +1,11 @@
 def rewrite(key, value):
-    with open(".env","r") as f:
-        lines = f.readlines()
-
-    with open(".env","w") as f:
-        for line in lines:
-            if key in line.split("=")[0]:
-                f.write("{}={}\n".format(line.split("=")[0], value))
-            else:
-                f.write(line)
+    try:
+        with open(".env","r+") as f:
+            for line in f.readlines():
+                if key in line.split("=")[0]:
+                    f.write("{}={}\n".format(line.split("=")[0], value))
+                else:
+                    f.write(line)
+    except Exception as e:
+        with open("log.txt","w") as f:
+            f.write(e)
